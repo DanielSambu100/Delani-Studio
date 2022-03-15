1b6ed0db88767c1ddd6d3efed79dabecbf27dea2
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-03-15 14:03:40 +0300

    update

---
+++ styles/jQuery.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+    $("#design-image").click(function (){
+$("#design-image").hide();
+$("#design").show();
+    });
+    $("#design").click(function(){
+        $("#design-image").toggle();
+        $("#design").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#development-image").click(function (){
+$("#development-image").hide();
+$("#development").show();
+    });
+    $("#develoment").click(function(){
+        $("#development-image").toggle();
+        $("#development").toggle();
+    });
+});
+
+$(document).ready(function(){
+    $("#product-image").click(function (){
+$("#product-image").hide();
+$("#product").show();
+    });
+    $("#product").click(function(){
+        $("#product-image").toggle();
+        $("#product").toggle();
+    });
+});

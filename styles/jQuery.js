ef2f938bff8cdec57869015941e1cfbfbd755465
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-03-15 14:34:38 +0300

    update jquery file

--- styles/jQuery.js
+++ styles/jQuery.js
@@ -1,9 +1,13 @@
+$("#design").hide();
+$("#development").hide();
+$("#product").hide();
 
 $(document).ready(function(){
     $("#design-image").click(function (){
-$("#design-image").hide();
-$("#design").show();
+        $("#design-image").hide();
+        $("#design").show();
     });
+
     $("#design").click(function(){
         $("#design-image").toggle();
         $("#design").toggle();
@@ -12,10 +16,11 @@ $("#design").show();
 
 $(document).ready(function(){
     $("#development-image").click(function (){
-$("#development-image").hide();
-$("#development").show();
+        $("#development-image").hide();
+        $("#development").show();
     });
-    $("#develoment").click(function(){
+
+    $("#development").click(function(){
         $("#development-image").toggle();
         $("#development").toggle();
     });
@@ -23,9 +28,10 @@ $("#development").show();
 
 $(document).ready(function(){
     $("#product-image").click(function (){
-$("#product-image").hide();
-$("#product").show();
+        $("#product-image").hide();
+        $("#product").show();
     });
+
     $("#product").click(function(){
         $("#product-image").toggle();
         $("#product").toggle();

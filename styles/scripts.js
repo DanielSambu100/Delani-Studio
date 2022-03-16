0c57ea40eabf9c7ce18891a66f27eb28dee79bbf
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-03-16 05:27:55 +0300

    update portfolio overlay

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,3 +1,4 @@
+// portfolio overlay
 $(document).ready(function(){
     $("#work1").mouseover(function(){
       $("#box1").show();
@@ -14,56 +15,39 @@ $(document).ready(function(){
   });
   $(document).ready(function(){
     $("#work3").mouseover(function(){
-      $("#overlay3").show();
+      $("#box3").show();
     }).mouseout(function(){
-      $("#overlay3").hide();
+      $("#box3").hide();
     });
   });
   $(document).ready(function(){
     $("#work4").mouseover(function(){
-      $("#overlay4").show();
+      $("#box4").show();
     }).mouseout(function(){
-      $("#overlay4").hide();
+      $("#box4").hide();
     });
   });
   
   $(document).ready(function(){
     $("#work5").mouseover(function(){
-      $("#overlay5").show();
+      $("#box5").show();
     }).mouseout(function(){
-      $("#overlay5").hide();
+      $("#box5").hide();
     });
     $("#work6").mouseover(function(){
-      $("#overlay6").show();
+      $("#box6").show();
     }).mouseout(function(){
-      $("#overlay6").hide();
+      $("#box6").hide();
     });
     $("#work7").mouseover(function(){
-      $("#overlay7").show();
+      $("#box7").show();
     }).mouseout(function(){
-      $("#overlay7").hide();
+      $("#box7").hide();
     });
     $("#work8").mouseover(function(){
-      $("#overlay8").show();
+      $("#box8").show();
     }).mouseout(function(){
-      $("#overlay8").hide();
+      $("#box8").hide();
     });
-  });
-  
-  $(document).ready(function(){
-    $("form#form34A").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#MERGE1").val();
-      var email = $("input#MERGE0").val();
-      var message = $("textarea#comment").val();
-      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
-      
-    });
-  
   });
   
